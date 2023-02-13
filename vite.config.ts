3e35d88166187e87ee2c91d23b6bef108579362a
七七 <1772014415@qq.com>
2023-02-13 23:44:32 +0800

    添加跨域、axios、拦截器、session本地存储、配置vite.config默认跨域, 安装axios、js-cookie、mitt

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: "localhost",
+    port: 8080,
+    open: true,
+    // 跨域的写法
+        proxy: {
+          '/api': {
+            target: 'http://43.143.229.251:8090', // 实际请求地址
+            changeOrigin: true, // 允许跨域
+            rewrite: (path) => path.replace(/^\/api/, ""),
+          },
+        },
+      },
+  resolve: {
+    alias: [
+      {
+        find: '@',                                   // 别名
+        replacement: resolve(__dirname, 'src'),      // 别名对应地址
+      },
+      {
+        find: 'components',
+        replacement: resolve(__dirname, 'src/components'),
+      }
+    ]
+  }
+  
 })
