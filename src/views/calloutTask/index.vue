6675dab4857cbd8ee50bae5df28e40e92a45b837
七七 <1772014415@qq.com>
2023-03-08 21:20:21 +0800

    更改书籍列表图片

--- src/views/calloutTask/index.vue
+++ src/views/calloutTask/index.vue
@@ -8,7 +8,7 @@
     >
       <el-card :body-style="{ padding: '0px' }">
         <img
-          src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+          src="../../assets/loginphoto.webp"
           class="image"
         />
         <div style="padding: 7px">
